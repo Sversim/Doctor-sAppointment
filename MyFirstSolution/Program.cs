98e985d79dbd794ed2dfcaee7ebf431c478e93b0
Sversim <Sversim@gmail.com>
2023-01-18 20:58:02 +1000

    Timetable Controller

--- MyFirstSolution/Program.cs
+++ MyFirstSolution/Program.cs
@@ -1,7 +1,6 @@
 using MyFirstClassLibrary;
 using DataBaseModerator;
 using Microsoft.EntityFrameworkCore;
-using MyFirstClassLibrary;
 using Domain;
 
 var builder = WebApplication.CreateBuilder(args);
@@ -12,8 +11,12 @@ builder.Services.AddDbContext<ApplicationContext>(options =>
     options.UseNpgsql($"Host=localhost;Port=5432;Database=DoctorsBase;Username=postgres;Password=post"));
 builder.Services.AddTransient<IUserRepository, UserRepository>();
 builder.Services.AddTransient<UserInteractor>();
+
 builder.Services.AddTransient<IMedicRepository, MedicRepository>();
 builder.Services.AddTransient<MedicInteractor>();
+
+builder.Services.AddTransient<ITimetableRepository, TimetableRepository>();
+builder.Services.AddTransient<TimetableInteractor>();
 
 
 builder.Services.AddControllers();
